830dca4fe5fc2224a9122c33a0333f60319986fc
andreigon <andreigon1337@gmail.com>
2023-05-18 20:49:09 +0300

    stable template

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'WallPost',
+  components: {
+
+
+  //  your component
+
+
+},
+
+  methods: {
+    
+  },
+})
+
+
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped lang="scss">
+
+</style>
